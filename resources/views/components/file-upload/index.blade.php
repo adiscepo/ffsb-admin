946296570ada3979477111318109a81cbb2f13c5
Attilio Discepoli <attilio.discepoli@ulb.be>
2026-06-05 19:16:24 +0200

    fix(file-upload): add JPEG extensions by default
    Signed-off-by: Attilio Discepoli <attilio.discepoli@ulb.be>

--- resources/views/components/file-upload/index.blade.php
+++ resources/views/components/file-upload/index.blade.php
@@ -40,7 +40,7 @@ new class extends Component {
         $this->folder_storage = $folder_storage;
     }
 
-    public function mount(array $formats = ['png', 'jpg', 'gif'], bool $multiple = false): void
+    public function mount(array $formats = ['png', 'jpg', 'jpeg', 'gif'], bool $multiple = false): void
     {
         $this->uuid = Str::uuid();
         $this->formats = $formats;
