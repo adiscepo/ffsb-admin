a6402a8a36cd5417543cd22a28f7056e8fefd470
Attilio Discepoli <attilio.discepoli@ulb.be>
2026-06-24 19:17:36 +0200

    fix(bug): layout overflow y scroll and width
    Signed-off-by: Attilio Discepoli <attilio.discepoli@ulb.be>

--- resources/views/pages/support/bugs/single.blade.php
+++ resources/views/pages/support/bugs/single.blade.php
@@ -74,105 +74,110 @@ new class extends Component {
 
 @include('partials.heading', ['route' => 'Support/Bugs/#' . $bug->id])
 
-<main class="mx-auto max-sm:p-5 md:w-9/12 py-5 space-y-4 overflow-y-scroll">
-    <h1 class="font-medium text-2xl">{{ $bug->title }} <span
-            class="text-zinc-500 font-light">#{{ $bug->id }}</span>
-    </h1>
-    @if ($bug->open)
-        <flux:badge variant="solid" color="zinc">Ouvert</flux:badge>
-    @else
-        <flux:badge variant="solid" color="green">Fermé</flux:badge>
-    @endif
-    <flux:separator />
-    <div class="grid md:grid-cols-[3fr_1fr] gap-y-5 md:gap-x-6">
-        <div class="flex flex-col gap-y-4">
-            <x-message :user="$bug->user">
-                <x-slot:header>
-                    Ouvert par <span class="text-zinc-800 dark:text-zinc-300 font-medium">{{ $bug->user->name }}</span>
-                    • {{ $bug->created_at->diffForHumans() }}
-                </x-slot>
-                <p class="text-zinc-800 dark:text-zinc-200 py-2" wire:model='$description'>{!! nl2br($bug->description) !!}</p>
-                @if ($bug->files_upload != null)
-                    @foreach ($bug->files_upload as $file)
-                        <img class="w-1/2" src="{{ Storage::url($file) }}" />
+<main class="overflow-y-scroll">
+    <div class="mx-auto max-sm:p-5 md:w-9/12 py-5 space-y-4">
+        <h1 class="font-medium text-2xl">{{ $bug->title }} <span
+                class="text-zinc-500 font-light">#{{ $bug->id }}</span>
+        </h1>
+        @if ($bug->open)
+            <flux:badge variant="solid" color="zinc">Ouvert</flux:badge>
+        @else
+            <flux:badge variant="solid" color="green">Fermé</flux:badge>
+        @endif
+        <flux:separator />
+        <div class="grid md:grid-cols-[3fr_1fr] gap-y-5 md:gap-x-6">
+            <div class="flex flex-col gap-y-4">
+                <x-message :user="$bug->user">
+                    <x-slot:header>
+                        Ouvert par <span
+                            class="text-zinc-800 dark:text-zinc-300 font-medium">{{ $bug->user->name }}</span>
+                        • {{ $bug->created_at->diffForHumans() }}
+                    </x-slot>
+                    <p class="text-zinc-800 dark:text-zinc-200 py-2" wire:model='$description'>{!! nl2br($bug->description) !!}
+                    </p>
+                    @if ($bug->files_upload != null)
+                        @foreach ($bug->files_upload as $file)
+                            <img class="w-1/2" src="{{ Storage::url($file) }}" />
+                        @endforeach
+                    @endif
+                </x-message>
+                <ol class="ml-20" data-timeline="" {{ $attributes }}>
+                    @foreach ($bug->events as $event)
+                        <x-timeline-event :event="$event" />
                     @endforeach
-                @endif
-            </x-message>
-            <ol class="ml-20" data-timeline="" {{ $attributes }}>
-                @foreach ($bug->events as $event)
-                    <x-timeline-event :event="$event" />
-                @endforeach
-            </ol>
-            <div class="flex gap-x-3 items-start">
-                <div class="w-full">
-                    @if ($bug->open)
-                        <div
-                            class="flex items-center gap-x-4 p-3 rounded-t-lg border border-zinc-300 dark:border-zinc-600 bg-zinc-100 dark:bg-zinc-600 w-full">
-                            <p class="text-sm text-zinc-500 dark:text-zinc-400">
-                                Ajouter un commentaire
-                            </p>
-                        </div>
-                        <div
-                            class="flex flex-col gap-y-2 p-2 border border-t-0 border-zinc-300 dark:border-zinc-600 rounded-b-lg">
-                            <textarea wire:model='comment' class="w-full h-20 resize-none p-2 text-sm focus-visible:ring-0!"
-                                placeholder="Entrez votre commentaire"></textarea>
-                            <div class="flex justify-end gap-x-2">
-                                <flux:checkbox.group wire:model="actions" variant="buttons">
-                                    <flux:checkbox value="close" icon="check-circle" icon:variant="outline"
-                                        icon:color="purple" label="Clôturer le bug" />
-                                </flux:checkbox.group>
-                                <flux:button variant="primary" color="violet" class="w-fit self-end"
-                                    wire:click='commentBug'>
-                                    Commenter</flux:button>
+                </ol>
+                <div class="flex gap-x-3 items-start">
+                    <div class="w-full">
+                        @if ($bug->open)
+                            <div
+                                class="flex items-center gap-x-4 p-3 rounded-t-lg border border-zinc-300 dark:border-zinc-600 bg-zinc-100 dark:bg-zinc-600 w-full">
+                                <p class="text-sm text-zinc-500 dark:text-zinc-400">
+                                    Ajouter un commentaire
+                                </p>
                             </div>
+                            <div
+                                class="flex flex-col gap-y-2 p-2 border border-t-0 border-zinc-300 dark:border-zinc-600 rounded-b-lg">
+                                <textarea wire:model='comment' class="w-full h-20 resize-none p-2 text-sm focus-visible:ring-0!"
+                                    placeholder="Entrez votre commentaire"></textarea>
+                                <div class="flex justify-end gap-x-2">
+                                    <flux:checkbox.group wire:model="actions" variant="buttons">
+                                        <flux:checkbox value="close" icon="check-circle" icon:variant="outline"
+                                            icon:color="purple" label="Clôturer le bug" />
+                                    </flux:checkbox.group>
+                                    <flux:button variant="primary" color="violet" class="w-fit self-end"
+                                        wire:click='commentBug'>
+                                        Commenter</flux:button>
+                                </div>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="space-y-5">
+                <div class="flex flex-col gap-2">
+                    <h2 class="text-zinc-500 font-medium">Tags</h2>
+                    <div class="flex gap-2 flex-wrap">
+                        @foreach ($bug->tags as $tag)
+                            <flux:badge color="{{ $tag->color }}">{{ $tag->name }}</flux:badge>
+                        @endforeach
+                    </div>
+                    @if ($bug->open)
+                        <div class="flex gap-x-2 items-end">
+                            <livewire:pill-box name="tags" :datas="Tag::for(Bug::class)->toArray()" :selected="$tags" />
                         </div>
                     @endif
                 </div>
-            </div>
-        </div>
-        <div class="space-y-5">
-            <div class="flex flex-col gap-2">
-                <h2 class="text-zinc-500 font-medium">Tags</h2>
-                <div class="flex gap-2 flex-wrap">
-                    @foreach ($bug->tags as $tag)
-                        <flux:badge color="{{ $tag->color }}">{{ $tag->name }}</flux:badge>
-                    @endforeach
-                </div>
-                @if ($bug->open)
-                    <div class="flex gap-x-2 items-end">
-                        <livewire:pill-box name="tags" :datas="Tag::for(Bug::class)->toArray()" :selected="$tags" />
+                <h2 class="text-zinc-500 font-medium">Assignation</h2>
+                @if ($bug->assignation != null and isset($bug->assignation))
+                    <div class="flex items-center justify-between text-zinc-600 dark:text-zinc-400">
+                        <div class="flex gap-x-3">
+                            <span class="flex items-center gap-x-2">
+                                <flux:avatar size="sm" circle src="{{ $bug->assignation->getProfilePicture() }}"
+                                    initials="{{ $bug->assignation->initials() }}" />
+                                {{ $bug->assignation->name }}
+                            </span>
+                        </div>
+                        @if ($bug->open)
+                            <flux:icon.trash wire:click='removeAssignation'
+                                class="cursor-pointer text-zinc-300 dark:text-zinc-700 hover:text-zinc-400 dark:hover:text-zinc-600 size-4"
+                                variant="outline" />
+                        @endif
                     </div>
-                @endif
-            </div>
-            <h2 class="text-zinc-500 font-medium">Assignation</h2>
-            @if ($bug->assignation != null and isset($bug->assignation))
-                <div class="flex items-center justify-between text-zinc-600 dark:text-zinc-400">
-                    <div class="flex gap-x-3">
-                        <span class="flex items-center gap-x-2">
-                            <flux:avatar size="sm" circle src="{{ $bug->assignation->getProfilePicture() }}"
-                                initials="{{ $bug->assignation->initials() }}" />
-                            {{ $bug->assignation->name }}
-                        </span>
-                    </div>
+                @else
                     @if ($bug->open)
-                        <flux:icon.trash wire:click='removeAssignation'
-                            class="cursor-pointer text-zinc-300 dark:text-zinc-700 hover:text-zinc-400 dark:hover:text-zinc-600 size-4"
-                            variant="outline" />
+                        <div class="flex gap-x-2">
+                            <flux:select wire:model='assignation'>
+                                @foreach (User::all() as $user)
+                                    <flux:select.option value="{{ $user->id }}">{{ $user->name }}
+                                    </flux:select.option>
+                                @endforeach
+                            </flux:select>
+                            <flux:button wire:click='assignTo'>Assigner</flux:button>
+                        </div>
                     @endif
-                </div>
-            @else
-                @if ($bug->open)
-                    <div class="flex gap-x-2">
-                        <flux:select wire:model='assignation'>
-                            @foreach (User::all() as $user)
-                                <flux:select.option value="{{ $user->id }}">{{ $user->name }}
-                                </flux:select.option>
-                            @endforeach
-                        </flux:select>
-                        <flux:button wire:click='assignTo'>Assigner</flux:button>
-                    </div>
                 @endif
-            @endif
+            </div>
         </div>
     </div>
+
 </main>
