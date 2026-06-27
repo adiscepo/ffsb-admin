ad4108d839d957a87e4ad58c34aa9d57d8f8fee9
Attilio Discepoli <attilio.discepoli@ulb.be>
2026-06-27 10:55:24 +0200

    feat(text-editor): components that use Quill.JS to redact html in an editor
    Signed-off-by: Attilio Discepoli <attilio.discepoli@ulb.be>

---
+++ resources/views/components/text-editor/index.blade.php
@@ -0,0 +1,51 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component {
+    const EVENT_VALUE_UPDATED = '';
+
+    public $value;
+
+    public string $quillId;
+    public string $placeholder;
+
+    public function mount($value = '', string $placeholder = '')
+    {
+        $this->value = $value;
+        $this->placeholder = $placeholder;
+        $this->quillId = 'quill-' . uniqid();
+    }
+
+    public function updatedValue($value)
+    {
+        $this->dispatch('text-editor-updated', $this->value);
+    }
+};
+?>
+
+<div {{ $attributes->merge(['class' => '']) }} wire:ignore>
+    <div class="mb-10" id="{{ $quillId }}"></div>
+    {{-- <input type="hidden" id="{{ $quillId . '-area' }}" value="{!! $value !!}" /> --}}
+    @push('scripts')
+        <script defer>
+            document.addEventListener('DOMContentLoaded', () => {
+                const editor = new Quill('#{{ $quillId }}', {
+                    placeholder: '{{ $placeholder }}',
+                    theme: 'snow',
+                });
+
+                const delta = editor.clipboard.convert({
+                    html: '{!! $value !!}'
+                });
+                editor.setContents(delta, 'silent')
+
+                editor.on('text-change', function() {
+                    let value = document.getElementsByClassName('ql-editor')[0].innerHTML;
+                    @this.set('value', value)
+                })
+
+            });
+        </script>
+    @endpush
+</div>
