ae27b401836b3762a9291f7ae1f7c4a5d1be0883
Attilio Discepoli <attilio.discepoli@ulb.be>
2026-07-10 22:00:30 +0200

    feat(bugs): number of comments
    Signed-off-by: Attilio Discepoli <attilio.discepoli@ulb.be>

--- app/Domains/Events/Traits/Eventable.php
+++ app/Domains/Events/Traits/Eventable.php
@@ -4,11 +4,19 @@ namespace App\Domains\Events\Traits;
 
 use App\Domains\Events\Event;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Support\Collection;
 
 trait Eventable
 {
     public function events(): MorphToMany
     {
         return $this->morphToMany(Event::class, 'eventable');
+    }
+
+    public function comments(): Collection
+    {
+        return $this->events->filter(function ($event) {
+            return $event->type == 'comment';
+        });
     }
 }
