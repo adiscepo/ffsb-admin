d0d085fd693597add1b0d255b5b8deb5f9e269aa
Attilio Discepoli <attilio.discepoli@ulb.be>
2026-07-15 00:25:54 +0200

    feat(dashboard): add illustrations of the 15th edition in welcome and dashboard
    Signed-off-by: Attilio Discepoli <attilio.discepoli@ulb.be>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-layouts::app :title="__('Tableau de bord')">
+<x-layouts::app :title="__('Tableau de bord')" class="relative">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl p-10">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative rounded-xl border border-neutral-200 dark:border-neutral-700">
@@ -13,8 +13,14 @@
                     class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+        {{-- <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-        </div>
+        </div> --}}
+    </div>
+    {{-- <div class="absolute bottom-2 right-10 w-1/8">
+        <img src="{{ url('/images/Pouf.png') }}" class="">
+    </div> --}}
+    <div class="absolute top-0 right-0 w-20">
+        <img src="{{ url('/images/RideauFurtif.png') }}" class="rotate-y-180">
     </div>
 </x-layouts::app>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,66 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'FFSB Admin') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <title>{{ config('app.name', 'FFSB Admin') }}</title>
 
-        @fonts
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 items-end flex-col h-screen">
-        <header class="flex justify-center items-center">
-        </header>
-        <main class="w-full h-full flex justify-center gap-10 items-center flex-col">
-            <span class="flex mb-1 items-center justify-center bg-accent rounded-full ">
-                <x-app-logo-icon class="size-42 m-6 text-white fill-current dark:text-white" />
-            </span>
-            @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
-                    @auth
-                        <a
-                            href="{{ route('dashboard') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            Dashboard
-                        </a>
-                    @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-[#1915014a] hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
-                            Se connecter
-                        </a>
+    @fonts
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-                        {{-- @if (Route::has('register'))
+<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 items-end flex-col h-screen">
+    <header class="flex justify-center items-center">
+    </header>
+    <main class="w-full h-full flex justify-center gap-10 items-center flex-col">
+        <span class="flex mb-1 items-center justify-center bg-accent rounded-full ">
+            <x-app-logo-icon class="size-42 m-6 text-white fill-current dark:text-white" />
+        </span>
+        @if (Route::has('login'))
+            <nav class="flex items-center justify-end gap-4">
+                @auth
+                    <a href="{{ route('dashboard') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                        Dashboard
+                    </a>
+                @else
+                    <a href="{{ route('login') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-[#1915014a] hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
+                        Se connecter
+                    </a>
+
+                    {{-- @if (Route::has('register'))
                             <a
                                 href="{{ route('register') }}"
                                 class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                            S'inscrire 
+                            S'inscrire
                             </a>
                         @endif --}}
-                    @endauth
-                </nav>
-            @endif
-        </main>
-
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
+                @endauth
+            </nav>
         @endif
-    </body>
+    </main>
+    <div class="absolute top-0 right-0 w-30 max-sm:hidden">
+        <img src="{{ url('/images/RideauFurtif.png') }}" class="rotate-y-180">
+    </div>
+    <div class="absolute top-0 left-5 w-70">
+        <img src="{{ url('/images/HamacCoupe.png') }}" class="translate-y-[-15pt]">
+    </div>
+    <div class="absolute bottom-2 right-5 w-60 max-sm:hidden">
+        <img src="{{ url('/images/Pouf.png') }}" class="">
+    </div>
+    <div class="absolute bottom-2 left-5 w-60  max-sm:hidden">
+        <img src="{{ url('/images/Transat.png') }}" class="">
+    </div>
+
+    @if (Route::has('login'))
+        <div class="h-14.5 hidden lg:block"></div>
+    @endif
+</body>
+
 </html>
