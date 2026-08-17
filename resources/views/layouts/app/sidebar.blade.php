ede9db2701bc3e2adc3160204669121f57594611
Attilio Discepoli <attilio.discepoli@ulb.be>
2026-08-17 15:45:55 +0200

    fix(sidebar): redirect to dashboard when clicked on ffsb logo (#37)
    Signed-off-by: Attilio Discepoli <attilio.discepoli@ulb.be>

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -9,7 +9,9 @@
     <x-debug-bar />
     <flux:sidebar sticky collapsible class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
         <flux:sidebar.header>
-            <x-app-logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate />
+            <a href="{{ route('dashboard') }}" wire:navigate>
+                <x-app-logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate />
+            </a>
             <flux:sidebar.collapse
                 class="in-data-flux-sidebar-on-desktop:not-in-data-flux-sidebar-collapsed-desktop:-mr-2" />
         </flux:sidebar.header>
