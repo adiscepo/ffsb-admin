91f20a25edce39a8493245f6f99363cba4b4c46f
Attilio Discepoli <attilio.discepoli@ulb.be>
2026-09-01 00:47:32 +0200

    feat(kanban): add first draft of index view
    Signed-off-by: Attilio Discepoli <attilio.discepoli@ulb.be>

---
+++ resources/views/pages/kanbans/index.blade.php
@@ -0,0 +1,50 @@
+<?php
+
+use Livewire\Component;
+use Livewire\Attributes\Computed;
+use Illuminate\Support\Collection;
+use App\Domains\Kanban\Kanban;
+
+new class extends Component {
+    public Collection $kanbans;
+
+    public function mount()
+    {
+        $this->kanbans = Kanban::all();
+    }
+};
+?>
+@component('partials.heading', ['route' => 'Kanbans'])
+    <div class="flex gap-x-2">
+        {{-- <livewire:docu.edit :docu="$docu" /> --}}
+        <flux:modal.trigger name="create-kanban">
+            <flux:button size="sm" variant="primary" color="violet" class="cursor-pointer hidden! md:block!">
+                Créer un kanban
+            </flux:button>
+            <flux:button size="sm" variant="primary" color="violet" class="cursor-pointer md:hidden" icon="plus">
+            </flux:button>
+        </flux:modal.trigger>
+    </div>
+@endcomponent
+
+<main class="flex flex-col gap-y-4 grow p-5">
+    <div>
+        <h2 class="text-lg text-zinc-700 dark:text-zinc-200">Kanbans</h2>
+        <h3 class="text-xs text-zinc-500"><span class="">カンバン</span> — Méthode de gestion des connaissances
+            relatives
+            au travail, où les différentes tâches en
+            cours sont affichées de façon visuelles.</h3>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+        @foreach ($kanbans as $kanban)
+            <div class="py-3 px-5 border w-full border-zinc-200 dark:border-zinc-800 dark:bg-zinc-700 rounded-xl hover:bg-zinc-50 dark:hover:bg-zinc-600 cursor-pointer space-y-3 max-sm:w-full relative"
+                wire:click='redirectProgram({{ $kanban->id }})'>
+                <div class="flex flex-col">
+                    <a>{{ $kanban->name }}</a>
+                    <span class="text-xs text-zinc-500 dark:text-zinc-300">{{ $kanban->description }}</span>
+                </div>
+                <span class="text-xs text-zinc-500 dark:text-zinc-300">Créé par {{ $kanban->user->name }}</span>
+            </div>
+        @endforeach
+    </div>
+</main>
